26d965843e53b756c74b4937ed066fb82f7713f6
Steve Trewick <steve.trewick@icloud.com>
2025-10-07 10:28:48 +0100

    update TerminalInput  package version after changes

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/SteveTrewick/TerminalInput",  from: "1.0.0"),
+        .package(url: "https://github.com/SteveTrewick/TerminalInput",  from: "1.0.1"),
         .package(url: "https://github.com/SteveTrewick/TerminalOutput", from: "1.0.0"),
     ],
     targets: [
